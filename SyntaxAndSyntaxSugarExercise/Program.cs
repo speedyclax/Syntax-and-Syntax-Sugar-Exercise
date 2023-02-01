13aa4cf02c5024efd224dc3bd0761594d0ed8fb7
Jon Claxton <speedyclax@gmail.com>
2023-02-01 15:40:32 -0600

    exercise complete

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? ($"{answer} is less then nine") : ($"{answer} is greater than or equal to nine");
+            Console.WriteLine(response);
         }
     }
 }
+
+
+//inferred typing
+//string interpolation
+//and the ternary operator
+/*      
+ *      int answer = 4;
+        string response;
+        if (answer < 9)
+        {
+            response = answer + " is less than nine";
+        }
+        else
+        {
+            response = answer + " is greater than or equal to nine";
+        }
+*/
